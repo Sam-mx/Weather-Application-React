39f83ce8862cb33ad77aa03c180ed7141b1ab18a
San Shwe <sanxhwe@gmail.com>
2024-03-14 22:16:39 +0630

    setups

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import search from './assets/icons/search.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='text-xl font-bold text-center'>
-     
+    <div className='w-full h-screen text-white px-8'>
+      <nav className='w-full p-3 flex justify-between items-center'>
+        <h1 className='font-bold tracking-wide text-3xl'>Weather Application</h1>
+          <div className='bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2'>
+            <img src={search} alt="search" className='w-[1.5rem] h-[1.5rem]' />
+
+            <input type='text' className='focus:outline-none w-full text-[#212121] text-lg'/>
+            
+          </div>
+      </nav>
     </div>
   )
 }
