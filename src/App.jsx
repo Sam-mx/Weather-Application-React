44d5b842a764ba4e4206ff6b460294f16cff37d6
San Shwe <sanxhwe@gmail.com>
2024-03-14 22:26:49 +0630

    setups

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import './App.css'
 
 function App() {
 
+  const [input, setInput] = useState('')
+
+  const submitCity = () => {
+    setPlace(input)
+    setInput('')
+  }
+
   return (
     <div className='w-full h-screen text-white px-8'>
       <nav className='w-full p-3 flex justify-between items-center'>
@@ -11,7 +18,13 @@ function App() {
           <div className='bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2'>
             <img src={search} alt="search" className='w-[1.5rem] h-[1.5rem]' />
 
-            <input type='text' className='focus:outline-none w-full text-[#212121] text-lg'/>
+            <input onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              // sumit the form
+              submitCity()
+            }
+          }} type="text" placeholder='Search city' className='focus:outline-none w-full text-[#212121] text-lg' value={input} onChange={e => setInput(e.target.value)} />
+        
             
           </div>
       </nav>
