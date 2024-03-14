741ab93aff29247af6b00546361d7832eb5f049f
San Shwe <sanxhwe@gmail.com>
2024-03-14 22:05:38 +0630

    initial setups

---
+++ src/components/BackgroundLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BackgroundLayout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BackgroundLayout
---
+++ src/components/MiniCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MiniCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MiniCard
